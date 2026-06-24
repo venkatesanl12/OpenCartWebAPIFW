db82a640b48f60acc7f8a447882c1332d5e9eb3a
venkatesanl12 <venkat.dwqa@gmail.com>
2026-06-24 15:35:00 +0530

    Config added for workflow file and PW Config TS files

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? '50%' : undefined,
 
   reporter: [
     ["list"],
@@ -35,6 +35,7 @@ export default defineConfig({
     video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: !process.env.CI ? false : true
   },
 
   /* Configure projects for major browsers */
